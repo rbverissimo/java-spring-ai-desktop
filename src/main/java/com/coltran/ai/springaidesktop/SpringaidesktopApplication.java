31013eec381fc78e1833182aa3f67f69a34e30b7
Renato Verissimo <rverissimo.dev@gmail.com>
2026-02-11 10:18:24 -0300

    first commit

---
+++ src/main/java/com/coltran/ai/springaidesktop/SpringaidesktopApplication.java
@@ -0,0 +1,13 @@
+package com.coltran.ai.springaidesktop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringaidesktopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringaidesktopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coltran/ai/springaidesktop/SpringaidesktopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coltran.ai.springaidesktop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringaidesktopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
